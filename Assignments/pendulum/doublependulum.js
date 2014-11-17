7412d7752ae35b2047af2ae619eef75d863465e9
jamers17 <james.kwapien@gmail.com>
2014-11-17 00:39:07 -0500

    Double Pendulum with Control Panel
    Working Control Panel with:
    -start button
    -stop button
    -reset button
    -mass controls
    -angle controls

--- Assignments/pendulum/doublependulum.js
+++ Assignments/pendulum/doublependulum.js
@@ -3,8 +3,10 @@ var d2Theta1 = 0;
 var d2Theta2 = 0;
 var dTheta1  = 0;
 var dTheta2  = 0;
-var Theta1   = 0*(Math.PI)/2;
-var Theta2   = 1*(Math.PI)/2;
+var theta1 	 = 0;
+var theta2   = 1;
+var Theta1   = theta1*(Math.PI)/2;
+var Theta2   = theta2*(Math.PI)/2;
 var mu 	   	 = 0;
 var m1 		 = 25;
 var m2       = 25;
@@ -16,8 +18,8 @@ var g        = 9.8;
 
 var stage = new Kinetic.Stage({
 	container: 'container',
-	width: 800,
-	height: 800
+	width: 796,
+	height: 796
 });
 
 // background layer
@@ -28,12 +30,12 @@ stage.add(layerOne);
 var grd = new Kinetic.Rect({
 	x: 0,
 	y: 0,
-	width: 800,
-	height: 800,
+	width: 796,
+	height: 796,
 	stroke: '#000',
-	strokeWidth: 5,
+	strokeWidth: 0,
 	fillLinearGradientStartPoint: {x: 0, y: 0},
-    fillLinearGradientEndPoint: {x: 800, y: 800},
+    fillLinearGradientEndPoint: {x: 796, y: 796},
     fillLinearGradientColorStops: [0, '#009DFF', 1, '#000DFF']
 });
 
@@ -54,7 +56,7 @@ stage.add(layerTwo);
 
 // first line instantiation
 var line1 = new Kinetic.Line({
-	points: [x0, y0, x0, y0 + l1],
+	// points: [x0, y0, x0, y0 + l1],
 	stroke: 'black',
 	strokeWidth: 5,
 	lineCap: 'round'
@@ -62,8 +64,8 @@ var line1 = new Kinetic.Line({
 
 // first circle instantiation
 var circle1 = new Kinetic.Circle({
-	x: x0,
-	y: y0 + l1,
+	// x: x0,
+	// y: y0 + l1,
 	radius: m1 / 2,
 	fill: 'orange',
 	stroke: 'black',
@@ -72,7 +74,7 @@ var circle1 = new Kinetic.Circle({
 
 // second line instantiation
 var line2 = new Kinetic.Line({
-	points: [x0, y0 + l1, x0, y0 + l1 + l2],
+	// points: [x0, y0 + l1, x0, y0 + l1 + l2],
 	stroke: 'black',
 	strokeWidth: 5,
 	lineCap: 'round'
@@ -80,19 +82,37 @@ var line2 = new Kinetic.Line({
 
 // second circle instantiation
 var circle2 = new Kinetic.Circle({
-	x: x0,
-	y: y0 + l1 + l2,
+	// x: x0,
+	// y: y0 + l1 + l2,
 	radius: m2 / 2,
 	fill: 'orange',
 	stroke: 'black',
 	strokeWidth: 1
 });
 
+
 layerTwo.add(line1);
 layerTwo.add(line2);
 layerTwo.add(circle1);
 layerTwo.add(circle2);
-layerTwo.draw();
+setAndDraw();
+
+function setAndDraw(){
+
+	// redraw both circles
+	circle1.setX(x0+l1*Math.sin(Theta1));
+    circle1.setY(y0+l1*Math.cos(Theta1));
+    circle1.setRadius(m1/2);
+    circle2.setX(x0+l1*Math.sin(Theta1)+l2*Math.sin(Theta2));
+    circle2.setY(y0+l1*Math.cos(Theta1)+l2*Math.cos(Theta2));
+    circle2.setRadius(m2/2);
+
+    // redraw both lines
+    line1.setPoints([x0, y0, circle1.getX(), circle1.getY()]);
+    line2.setPoints([circle1.getX(), circle1.getY(), circle2.getX(), circle2.getY()]);
+
+    layerTwo.draw();
+};
 
 // calculation routines and animation function which is called repeatedly
 var anim = new Kinetic.Animation(function(frame) {
@@ -108,6 +128,9 @@ var anim = new Kinetic.Animation(function(frame) {
 	Theta1    += dTheta1 * t;
 	Theta2    += dTheta2 * t;
 
+	var xStep = circle1.getX();
+	var yStep = circle1.getY();
+
 	// redraw both circles
 	circle1.setX(x0+l1*Math.sin(Theta1));
     circle1.setY(y0+l1*Math.cos(Theta1));
@@ -121,4 +144,93 @@ var anim = new Kinetic.Animation(function(frame) {
 }, layerTwo);
 
 // calls animation routine
-anim.start();
+document.getElementById("startButton").addEventListener("click", function(){
+	anim.start();
+});
+
+// stops animation routine
+document.getElementById("stopButton").addEventListener("click", function(){
+	anim.stop();
+});
+
+// resets variables to defaults, redraws and defaults field values
+document.getElementById("resetButton").addEventListener("click", function(){
+	
+	// re-initializes variables to default state
+	d2Theta1 = 0;
+	d2Theta2 = 0;
+	dTheta1  = 0;
+	dTheta2  = 0;
+	theta1 	 = 0;
+	theta2   = 1;
+	Theta1   = theta1*(Math.PI)/2;
+	Theta2   = theta2*(Math.PI)/2;
+	mu 	   	 = 0;
+	m1 		 = 25;
+	m2       = 25;
+	l1       = 150;
+	l2       = 150;
+	x0       = 400;
+	y0       = 350;
+	g        = 9.8;
+
+	// redraws elements
+	setAndDraw();
+
+	// defaults HTML input field and slider values
+	document.getElementById("m1Input").value = 25;
+	document.getElementById("m1Slider").value = 25;
+	document.getElementById("m2Input").value = 25;
+	document.getElementById("m2Slider").value = 25;
+	document.getElementById("a1Input").value = 0;
+	document.getElementById("a1Slider").value = 0;
+	document.getElementById("a2Input").value = 1;
+	document.getElementById("a2Slider").value = 1;
+
+});
+
+// changes m1 variable
+document.getElementById("m1Input").addEventListener("input", function(){
+	m1 = document.getElementById("m1Input").value;
+	setAndDraw();
+});
+
+document.getElementById("m1Slider").addEventListener("change", function(){
+	m1 = document.getElementById("m1Slider").value;
+	setAndDraw();
+});
+
+// changes m2 variable
+document.getElementById("m2Input").addEventListener("input", function(){
+	m2 = document.getElementById("m2Input").value;
+	setAndDraw();
+});
+
+document.getElementById("m2Slider").addEventListener("change", function(){
+	m2 = document.getElementById("m2Slider").value;
+	setAndDraw();
+});
+
+// changes theta1 variable
+document.getElementById("a1Input").addEventListener("input", function(){
+	theta1 = document.getElementById("a1Input").value;
+	Theta1 = theta1*(Math.PI)/2;
+	setAndDraw();
+});
+
+document.getElementById("a1Slider").addEventListener("change", function(){
+	theta1 = document.getElementById("a1Slider").value;
+	Theta2 = theta2*(Math.PI)/2;
+	setAndDraw();
+});
+
+// changes theta2 variable
+document.getElementById("a2Input").addEventListener("input", function(){
+	theta2 = document.getElementById("a2Input").value;
+	setAndDraw();
+});
+
+document.getElementById("a2Slider").addEventListener("change", function(){
+	theta2 = document.getElementById("a2Slider").value;
+	setAndDraw();
+});

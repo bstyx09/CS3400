12ff0d939b92ebb92c335e7221ae8ad67662adac
jamers17 <james.kwapien@gmail.com>
2014-11-18 15:48:05 -0500

    Added Circle Visibility and draw function
    Hopefully increased efficiency of redraw function

--- Assignments/pendulum/doublependulum.js
+++ Assignments/pendulum/doublependulum.js
@@ -1,4 +1,5 @@
-// physics constants
+/** PHYSICS CONSTANTS THAT ARE UPDATED DYNAMICALLY AFTER INSTANTIATION *******/
+
 var d2Theta1 = 0;
 var d2Theta2 = 0;
 var dTheta1  = 0;
@@ -15,6 +16,8 @@ var l2       = 150;
 var x0       = 400;
 var y0       = 350;
 var g        = 9.8;
+
+/** THE KINETICJS STAGE AND BACKGROUND LAYER AND ELEMENTS FOLLOW *************/
 
 var stage = new Kinetic.Stage({
 	container: 'container',
@@ -39,6 +42,7 @@ var grd = new Kinetic.Rect({
     fillLinearGradientColorStops: [0, '#009DFF', 1, '#000DFF']
 });
 
+// double pendulum base for stylistic reasons only
 var base = new Kinetic.Line({
 	points: [x0-1, y0, x0+1, y0],
 	stroke: 'black',
@@ -46,6 +50,7 @@ var base = new Kinetic.Line({
 	lineCap: 'round'
 });
 
+// adds background elements to base layer
 layerOne.add(grd);
 layerOne.add(base);
 layerOne.draw();
@@ -53,6 +58,8 @@ layerOne.draw();
 // animated layer
 var layerTwo = new Kinetic.Layer();
 stage.add(layerTwo);
+
+/** ELEMENTS THAT ARE DRAWN AND UPDATED FOLLOW *******************************/
 
 // first line instantiation
 var line1 = new Kinetic.Line({
@@ -90,16 +97,23 @@ var circle2 = new Kinetic.Circle({
 	strokeWidth: 1
 });
 
+var trail1 = new Kinetic.Path({
+	stroke: black,
+	
+});
 
+// adds dynamic elements to animated layer
 layerTwo.add(line1);
 layerTwo.add(line2);
 layerTwo.add(circle1);
 layerTwo.add(circle2);
 setAndDraw();
 
+/** FUNCTION REDRAWS ELEMENTS DYNAMICALLY WHEN PROMPTED BY EVENT HANDLERS ****/
+
 function setAndDraw(){
 
-	// redraw both circles
+	// recalculate both circles
 	circle1.setX(x0+l1*Math.sin(Theta1));
     circle1.setY(y0+l1*Math.cos(Theta1));
     circle1.setRadius(m1/2);
@@ -107,32 +121,19 @@ function setAndDraw(){
     circle2.setY(y0+l1*Math.cos(Theta1)+l2*Math.cos(Theta2));
     circle2.setRadius(m2/2);
 
-    // redraw both lines
+    // recalculate both lines
     line1.setPoints([x0, y0, circle1.getX(), circle1.getY()]);
     line2.setPoints([circle1.getX(), circle1.getY(), circle2.getX(), circle2.getY()]);
 	
-	if(document.getElementById('line1Set').checked) 
-	{
-		line1.visible(true);
-	}else
-	{
-		line1.visible(false);
-	}
-	
-	if(document.getElementById('line2Set').checked) 
-	{
-		line2.visible(true);
-	}else
-	{
-		line2.visible(false);
-	}
-	
+	// redraws dynamic elements
     layerTwo.draw();
 };
 
-// calculation routines and animation function which is called repeatedly
+/** REPEATEDLY CALLED CALCULATIONS AND SUBSEQUENT ANIMATION ******************/
+
 var anim = new Kinetic.Animation(function(frame) {
 
+	// local time variable set to the difference between animation updates
 	var t = frame.timeDiff * 0.01;
 
 	// calculations from formula, no friction present
@@ -144,20 +145,20 @@ var anim = new Kinetic.Animation(function(frame) {
 	Theta1    += dTheta1 * t;
 	Theta2    += dTheta2 * t;
 
-	var xStep = circle1.getX();
-	var yStep = circle1.getY();
-
-	// redraw both circles
+	// recalculate both circles
 	circle1.setX(x0+l1*Math.sin(Theta1));
     circle1.setY(y0+l1*Math.cos(Theta1));
     circle2.setX(x0+l1*Math.sin(Theta1)+l2*Math.sin(Theta2));
     circle2.setY(y0+l1*Math.cos(Theta1)+l2*Math.cos(Theta2));
 
-    // redraw both lines
+    // recalculate both lines
     line1.setPoints([x0, y0, circle1.getX(), circle1.getY()]);
     line2.setPoints([circle1.getX(), circle1.getY(), circle2.getX(), circle2.getY()]);
 
+    // final argument of the function indicates which layer to redraw
 }, layerTwo);
+
+/** EVENT LISTENERS AND HANDLERS FOLLOW **************************************/
 
 // calls animation routine
 document.getElementById("startButton").addEventListener("click", function(){
@@ -205,7 +206,8 @@ document.getElementById("resetButton").addEventListener("click", function(){
 
 });
 
-// changes m1 variable
+/** CHANGES M1 VARIABLE ******************************************************/
+
 document.getElementById("m1Input").addEventListener("input", function(){
 	m1 = document.getElementById("m1Input").value;
 	setAndDraw();
@@ -216,7 +218,8 @@ document.getElementById("m1Slider").addEventListener("change", function(){
 	setAndDraw();
 });
 
-// changes m2 variable
+/** CHANGES M2 VARIABLE ******************************************************/
+
 document.getElementById("m2Input").addEventListener("input", function(){
 	m2 = document.getElementById("m2Input").value;
 	setAndDraw();
@@ -227,7 +230,8 @@ document.getElementById("m2Slider").addEventListener("change", function(){
 	setAndDraw();
 });
 
-// changes theta1 variable
+/** CHANGES THETA1 VARIABLE **************************************************/
+
 document.getElementById("a1Input").addEventListener("input", function(){
 	theta1 = document.getElementById("a1Input").value;
 	Theta1 = theta1*(Math.PI)/2;
@@ -240,7 +244,8 @@ document.getElementById("a1Slider").addEventListener("change", function(){
 	setAndDraw();
 });
 
-// changes theta2 variable
+/** CHANGES THETA2 VARIABLE **************************************************/
+
 document.getElementById("a2Input").addEventListener("input", function(){
 	theta2 = document.getElementById("a2Input").value;
 	Theta2 = theta2*(Math.PI)/2;
@@ -252,6 +257,56 @@ document.getElementById("a2Slider").addEventListener("change", function(){
 	Theta2 = theta2*(Math.PI)/2;
 	setAndDraw();
 });
+
+
+/** LINE VISIBILITY CHECKS ***************************************************/
+document.getElementById("line1Set").addEventListener("change", function(){
+	if(document.getElementById("line1Set").checked) 
+	{
+		line1.visible(true);
+	}else
+	{
+		line1.visible(false);
+	}
+	setAndDraw();
+});
+
+document.getElementById("line2Set").addEventListener("change", function(){
+	if(document.getElementById("line2Set").checked) 
+	{
+		line2.visible(true);
+	}else
+	{
+		line2.visible(false);
+	}
+	setAndDraw();
+});
+
+/** CIRCLE VISIBILITY CHECKS *************************************************/
+
+document.getElementById("circle1Set").addEventListener("change", function(){
+	if(document.getElementById("circle1Set").checked) 
+	{
+		circle1.visible(true);
+	}else
+	{
+		circle1.visible(false);
+	}
+	setAndDraw();
+});
+
+document.getElementById("circle2Set").addEventListener("change", function(){
+	if(document.getElementById("circle2Set").checked) 
+	{
+		circle2.visible(true);
+	}else
+	{
+		circle2.visible(false);
+	}
+	setAndDraw();
+});
+
+/** FILE I/O HANDLING ********************************************************/
 
 function fileLoad(){
 	m1 = document.getElementById("m1Input").value;
